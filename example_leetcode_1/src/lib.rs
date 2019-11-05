4a5cf137a0a711d9054f78fcba67fc255d03d38a
kumatata <chinagxwei@gmail.com>
2019-11-05 09:50:18 +0800

    add topic

--- example_leetcode_1/src/lib.rs
+++ example_leetcode_1/src/lib.rs
@@ -161,7 +161,9 @@ pub fn length_of_longest_substring(s: String) -> i32 {
 /// 链接：https://leetcode-cn.com/problems/median-of-two-sorted-arrays
 /// 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 
+///
 /// 算法实现
+///
 pub fn find_median_sorted_arrays_1(mut nums1: Vec<i32>, mut nums2: Vec<i32>) -> f64 {
     let (mut n1_len, mut n2_len) = (nums1.len(), nums2.len());
     if n1_len > n2_len {
@@ -209,7 +211,9 @@ pub fn find_median_sorted_arrays_1(mut nums1: Vec<i32>, mut nums2: Vec<i32>) -> 
     0.0
 }
 
+///
 /// 标准库实现
+///
 pub fn find_median_sorted_arrays_2(mut nums1: Vec<i32>, mut nums2: Vec<i32>) -> f64 {
     fn get_median(v: Vec<i32>) -> f64 {
         let len = v.len();
@@ -252,8 +256,59 @@ pub fn find_median_sorted_arrays_2(mut nums1: Vec<i32>, mut nums2: Vec<i32>) -> 
 /// 链接：https://leetcode-cn.com/problems/longest-palindromic-substring
 /// 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 
-pub fn longest_palindrome(s: String) -> String {
-    String::from("")
+///
+/// 马拉车算法
+///
+pub fn longest_palindrome(mut s: String) -> String {
+    if s.len() < 1 || s.len() > 1000 { return "".to_string(); }
+    let str_format: String = {
+        let mut str_vec = {
+            s.chars().map(|ch| { ch.to_string() }).collect::<Vec<String>>()
+        };
+        str_vec.insert(0, "^".to_string());
+        str_vec.push("$".to_string());
+        str_vec.join("#")
+    };
+    let sss_len = str_format.len();
+    let mut p: Vec<usize> = vec![0; sss_len];
+    //c表示中心下标，r表示右侧半径
+    let (mut c, mut r) = (0 as i32, 0);
+    for (index, _) in str_format.chars().enumerate() {
+        if index == 0 || index == (sss_len - 1) { continue; };
+        let mirror: usize = (2 * c - index as i32) as usize;
+
+        if r > (index) {
+            p[index] = (r - index).min(p[mirror]);
+        } else {
+            p[index] = 0;
+        }
+
+        //判断中心字符两侧一样字符的数量
+        let (mut left, mut right) = (index + 1 + p[index], index - 1 - p[index]);
+        while str_format.get(left..left + 1).unwrap() == str_format.get(right..right + 1).unwrap() {
+            p[index] += 1;
+            left = index + 1 + p[index];
+            right = index - 1 - p[index];
+        }
+
+        //判断是否移动记录的下标中心和更新右侧半径
+        if (index + p[index]) > r {
+            c = index as i32;
+            r = index + p[index];
+        }
+    }
+
+    //获取最大长度，和对应下标
+    let (mut max_len, mut center_index) = (0, 0);
+    for (i, v) in p.iter().enumerate() {
+        if *v > max_len {
+            max_len = *v;
+            center_index = i;
+        }
+    }
+    //求源字符串开头下标
+    let start = (center_index - max_len) / 2;
+    s.get(start..start + max_len).unwrap().to_string()
 }
 
 #[cfg(test)]
