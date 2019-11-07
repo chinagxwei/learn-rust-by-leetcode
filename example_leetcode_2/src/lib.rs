d57c9d76fdbb530122955cbd04f9df2a07d4b14a
kumatata <chinagxwei@gmail.com>
2019-11-07 11:11:02 +0800

    add topic

--- example_leetcode_2/src/lib.rs
+++ example_leetcode_2/src/lib.rs
@@ -32,7 +32,7 @@
 ///
 
 pub fn convert(s: String, num_rows: i32) -> String {
-    if num_rows == 1 { return s }
+    if num_rows == 1 { return s; }
     let take = num_rows.min(s.len() as i32) as usize;
     let mut rows = vec![String::new(); take];
     let (mut cur_row, mut going_down, mut ret) = (0_i32, false, String::new());
@@ -47,13 +47,55 @@ pub fn convert(s: String, num_rows: i32) -> String {
     ret
 }
 
+///
+/// 给出一个 32 位的有符号整数，你需要将这个整数中每位上的数字进行反转。
+///
+/// 示例 1:
+///
+/// 输入: 123
+/// 输出: 321
+/// 示例 2:
+///
+/// 输入: -123
+/// 输出: -321
+/// 示例 3:
+///
+/// 输入: 120
+/// 输出: 21
+/// 注意:
+///
+/// 假设我们的环境只能存储得下 32 位的有符号整数，则其数值范围为 [−231,  231 − 1]。请根据这个假设，如果反转后整数溢出那么就返回 0。
+///
+/// 来源：力扣（LeetCode）
+/// 链接：https://leetcode-cn.com/problems/reverse-integer
+/// 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+///
+
+pub fn reverse(mut x: i32) -> i32 {
+    let mut res = 0_i64;
+    while x != 0 {
+        let pop = x % 10;
+        if res > (std::i32::MAX / 10) as i64 || (res == (std::i32::MAX / 10) as i64 && pop > 7) { return 0; }
+        if res < (std::i32::MIN / 10) as i64 || (res == (std::i32::MIN / 10) as i64 && pop < -8) { return 0; }
+        res = res * 10 + pop as i64;
+        x /= 10;
+    }
+    res as i32
+}
+
 #[cfg(test)]
 mod test {
-    use crate::convert;
+    use crate::{convert, reverse};
 
     #[test]
     fn test_convert() {
         let res = convert("LEETCODEISHIRING".to_string(), 3);
         assert_eq!(res, String::from("LCIRETOESIIGEDHN"))
+    }
+
+    #[test]
+    fn test_revers() {
+        let res = reverse(123);
+        assert_eq!(res, 321)
     }
 }
