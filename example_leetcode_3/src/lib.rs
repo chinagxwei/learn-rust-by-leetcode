389386aeec9dbf01737bb6e4b62e3e4c68fc1f27
kumatata <chinagxwei@gmail.com>
2019-11-13 10:37:29 +0800

    add topic

--- example_leetcode_3/src/lib.rs
+++ example_leetcode_3/src/lib.rs
@@ -200,9 +200,52 @@ pub fn roman_to_int(s: String) -> i32 {
     num
 }
 
+/// 14.
+///
+/// 编写一个函数来查找字符串数组中的最长公共前缀。
+///
+/// 如果不存在公共前缀，返回空字符串 ""。
+///
+/// 示例 1:
+///
+/// 输入: ["flower","flow","flight"]
+/// 输出: "fl"
+/// 示例 2:
+///
+/// 输入: ["dog","racecar","car"]
+/// 输出: ""
+/// 解释: 输入不存在公共前缀。
+/// 说明:
+///
+/// 所有输入只包含小写字母 a-z 。
+///
+/// 来源：力扣（LeetCode）
+/// 链接：https://leetcode-cn.com/problems/longest-common-prefix
+/// 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+///
+
+pub fn longest_common_prefix(strs: Vec<String>) -> String {
+    if strs.len() == 0 { return String::from("") }
+    let mut ans = *&strs[0].as_str();
+    for s in strs.iter() {
+        let mut i = 0;
+        while i < ans.len() && i < s.len() {
+            if ans.get(i..=i).unwrap() != s.get(i..=i).unwrap() {
+                break;
+            }
+            i += 1;
+        }
+        ans = ans.get(0..i).unwrap();
+        if ans == "" {
+            return String::from(ans)
+        }
+    }
+    String::from(ans)
+}
+
 #[cfg(test)]
 mod test {
-    use crate::{max_area, int_to_roman_1, int_to_roman_2, roman_to_int};
+    use crate::{max_area, int_to_roman_1, int_to_roman_2, roman_to_int, longest_common_prefix};
 
     #[test]
     fn test_max_area() {
@@ -226,5 +269,13 @@ mod test {
         assert_eq!(res, 58);
         let res = roman_to_int("MCMXCIV".to_string());
         assert_eq!(res, 1994);
+    }
+
+    #[test]
+    fn test_longest_common_prefix() {
+        let res = longest_common_prefix(["flower".to_string(), "flow".to_string(), "flight".to_string()].to_vec());
+        assert_eq!(res, "fl".to_string());
+        let res = longest_common_prefix(["dog".to_string(), "racecar".to_string(), "car".to_string()].to_vec());
+        assert_eq!(res, "".to_string());
     }
 }
