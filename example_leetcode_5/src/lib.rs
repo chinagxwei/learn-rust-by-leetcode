59bf285c373efe91f58704cfdcad38bd145b3eda
kumatata <chinagxwei@gmail.com>
2019-11-21 10:47:22 +0800

    add topic

--- example_leetcode_5/src/lib.rs
+++ example_leetcode_5/src/lib.rs
@@ -74,23 +74,102 @@ pub fn merge_two_lists_2(l1: Option<Box<ListNode>>, l2: Option<Box<ListNode>>) -
     dummy.next
 }
 
+/// 22.
+///
+/// 给出 n 代表生成括号的对数，请你写出一个函数，使其能够生成所有可能的并且有效的括号组合。
+///
+/// 例如，给出 n = 3，生成结果为：
+///
+/// [
+///   "((()))",
+///   "(()())",
+///   "(())()",
+///   "()(())",
+///   "()()()"
+/// ]
+///
+/// 来源：力扣（LeetCode）
+/// 链接：https://leetcode-cn.com/problems/generate-parentheses
+/// 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+///
+
+/// 回溯法
+pub fn generate_parenthesis(n: i32) -> Vec<String> {
+    fn backtrack(ans: &mut Vec<String>, current: &str, open: i32, close: i32, max: i32) {
+        if current.len() == (max * 2) as usize {
+            ans.push(current.to_string())
+        } else {
+            if open < max {
+                let mut cur = current.to_string();
+                cur.push_str("(");
+                backtrack(ans, &cur, open + 1, close, max)
+            }
+            if close < open {
+                let mut cur = current.to_string();
+                cur.push_str(")");
+                backtrack(ans, &cur, open, close + 1, max)
+            }
+        }
+    }
+    let mut ans = vec![];
+    backtrack(&mut ans, "", 0, 0, n);
+    ans
+}
+
 #[cfg(test)]
 mod test {
-    use crate::{ListNode, merge_two_lists_1, merge_two_lists_2};
+    use crate::{ListNode, merge_two_lists_1, merge_two_lists_2, generate_parenthesis};
 
     #[test]
     fn test_merge_two_lists() {
         let (l1, l2) = (
-            Some(Box::new(ListNode { val: 1, next: Some(Box::new(ListNode { val: 2, next: Some(Box::new(ListNode { val: 4, next: None })) })) })),
-            Some(Box::new(ListNode { val: 1, next: Some(Box::new(ListNode { val: 3, next: Some(Box::new(ListNode { val: 4, next: None })) })) }))
+            Some(Box::new(ListNode { val: 1, next: Some(Box::new(ListNode { val: 2, next: None })) })),
+            Some(Box::new(ListNode { val: 2, next: Some(Box::new(ListNode { val: 3, next: None })) }))
         );
         let res = merge_two_lists_1(l1, l2);
-        println!("{:?}", res);
+        assert_eq!(
+            res,
+            Some(Box::new(ListNode {
+                val: 1,
+                next:
+                Some(Box::new(ListNode {
+                    val: 2,
+                    next:
+                    Some(Box::new(ListNode {
+                        val: 2,
+                        next:
+                        Some(Box::new(ListNode { val: 3, next: None })),
+                    })),
+                })),
+            }))
+        );
         let (l1, l2) = (
-            Some(Box::new(ListNode { val: 1, next: Some(Box::new(ListNode { val: 3, next: Some(Box::new(ListNode { val: 5, next: None })) })) })),
-            Some(Box::new(ListNode { val: 1, next: Some(Box::new(ListNode { val: 2, next: Some(Box::new(ListNode { val: 3, next: None })) })) }))
+            Some(Box::new(ListNode { val: 1, next: Some(Box::new(ListNode { val: 3, next: None })) })),
+            Some(Box::new(ListNode { val: 1, next: Some(Box::new(ListNode { val: 2, next: None })) }))
         );
         let res = merge_two_lists_2(l1, l2);
-        println!("{:?}", res)
+        assert_eq!(
+            res,
+            Some(Box::new(ListNode {
+                val: 1,
+                next:
+                Some(Box::new(ListNode {
+                    val: 1,
+                    next:
+                    Some(Box::new(ListNode {
+                        val: 2,
+                        next:
+                        Some(Box::new(ListNode { val: 3, next: None })),
+                    })),
+                })),
+            }))
+        );
+    }
+
+    #[test]
+    fn test_generate_parenthesis() {
+        let res = generate_parenthesis(2);
+        println!("{:?}", res);
+        assert_eq!(res,["(())", "()()"])
     }
 }
