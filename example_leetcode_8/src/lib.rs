c4c8ba8f75904d58ae6a998bd1fed3094d50f922
kumatata <chinagxwei@gmail.com>
2019-12-12 15:59:17 +0800

    add topic

--- example_leetcode_8/src/lib.rs
+++ example_leetcode_8/src/lib.rs
@@ -142,9 +142,9 @@ pub fn solve_sudoku_1(board: &mut Vec<Vec<char>>) {
 
 /// dfs + 剪枝 + 位运算
 pub fn solve_sudoku_2(board: &mut Vec<Vec<char>>) {
-    let (mut row, mut column) = (vec![(1 << 9) - 1; 9], vec![(1 << 9) - 1; 9]);
-    let (mut ones, mut map) = (vec![0; 1 << 9], vec![0_i32; 1 << 9]);
-    let mut cell = vec![vec![(1 << 9) - 1; 3]; 3];
+    let (mut row, mut column) = (vec![511; 9], vec![511; 9]);
+    let (mut ones, mut map) = (vec![0; 512], vec![-1_i32; 512]);
+    let mut cell = vec![vec![511; 3]; 3];
     type M = Vec<i32>;
     type N<T> = Vec<Vec<T>>;
     fn low_bit(x: i32) -> i32 { -x & x };
@@ -213,7 +213,7 @@ pub fn solve_sudoku_2(board: &mut Vec<Vec<char>>) {
         map[1 << i] = i as i32;
     }
 
-    for i in 0..1 << 9 {
+    for i in 0..512 {
         let mut n = 0;
         let mut j = i;
         while j != 0 {
@@ -226,9 +226,73 @@ pub fn solve_sudoku_2(board: &mut Vec<Vec<char>>) {
     dfs(board, &mut row, &mut column, &mut cell, &mut ones, &mut map, cnt);
 }
 
+/// 38.
+///
+/// 报数序列是一个整数序列，按照其中的整数的顺序进行报数，得到下一个数。其前五项如下：
+///
+/// 1.     1
+/// 2.     11
+/// 3.     21
+/// 4.     1211
+/// 5.     111221
+/// 1 被读作  "one 1"  ("一个一") , 即 11。
+/// 11 被读作 "two 1s" ("两个一"）, 即 21。
+/// 21 被读作 "one 2",  "one 1" （"一个二" ,  "一个一") , 即 1211。
+///
+/// 给定一个正整数 n（1 ≤ n ≤ 30），输出报数序列的第 n 项。
+///
+/// 注意：整数顺序将表示为一个字符串。
+///
+///  
+///
+/// 示例 1:
+///
+/// 输入: 1
+/// 输出: "1"
+/// 示例 2:
+///
+/// 输入: 4
+/// 输出: "1211"
+///
+/// 来源：力扣（LeetCode）
+/// 链接：https://leetcode-cn.com/problems/count-and-say
+/// 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+///
+pub fn count_and_say(n: i32) -> String {
+    let (mut num_str, mut result) = (String::from(""), String::from("1"));
+    for i in 1..n {
+        num_str = result;
+        result = String::from("");
+        let mut current_num = 0;
+        while current_num < num_str.len() {
+            let (mut count, mut next_num) = (0, current_num);
+            while next_num < num_str.len() && &num_str[current_num..=current_num] == &num_str[next_num..=next_num] {
+                next_num += 1;
+                count += 1;
+            }
+            result.push(count.to_string().parse().unwrap());
+            result.push_str(&num_str[current_num..=current_num]);
+            current_num = next_num;
+        }
+    }
+    String::from(result)
+}
+
+pub fn count_and_say_2(n: i32) -> String {
+    let a = String::from("1211");
+    let mut start = 0;
+    let mut end = 0;
+
+    while start < a.len() {}
+
+
+    String::from("result")
+}
+
+
 #[cfg(test)]
 mod test {
-    use crate::{is_valid_sudoku, solve_sudoku_1, solve_sudoku_2};
+    use crate::{is_valid_sudoku, solve_sudoku_1, solve_sudoku_2, count_and_say};
 
     #[test]
     fn test_is_valid_sudoku() {
@@ -274,6 +338,15 @@ mod test {
             vec!['.', '.', '.', '.', '8', '.', '.', '7', '9']
         ];
         solve_sudoku_2(&mut board);
+    }
 
+    #[test]
+    fn test_count_and_say() {
+        let res = count_and_say(4);
+        assert_eq!(res, String::from("1211"));
+        let res = count_and_say(5);
+        assert_eq!(res, String::from("111221"));
+        let res = count_and_say(6);
+        assert_eq!(res, String::from("312211"));
     }
 }
