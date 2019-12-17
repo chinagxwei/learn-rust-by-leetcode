b4570547c8de913b3366704a012021a9b04588a3
kumatata <chinagxwei@gmail.com>
2019-12-17 15:28:36 +0800

    add topic

--- example_leetcode_9/src/lib.rs
+++ example_leetcode_9/src/lib.rs
@@ -40,6 +40,46 @@ pub fn first_missing_positive(nums: Vec<i32>) -> i32 {
     nums_len as i32 + 1
 }
 
+/// 42.
+///
+/// 给定 n 个非负整数表示每个宽度为 1 的柱子的高度图，计算按此排列的柱子，下雨之后能接多少雨水。
+///
+///
+///
+/// 上面是由数组 [0,1,0,2,1,0,1,3,2,1,2,1] 表示的高度图，在这种情况下，可以接 6 个单位的雨水（蓝色部分表示雨水）。 感谢 Marcos 贡献此图。
+///
+/// 示例:
+///
+/// 输入: [0,1,0,2,1,0,1,3,2,1,2,1]
+/// 输出: 6
+///
+/// 来源：力扣（LeetCode）
+/// 链接：https://leetcode-cn.com/problems/trapping-rain-water
+/// 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+///
+pub fn trap(height: Vec<i32>) -> i32 {
+    if height.len() == 0 { return 0; }
+    let (mut left, mut right, mut ans, mut left_max, mut right_max) = (0, height.len() - 1, 0, 0, 0);
+    while left < right {
+        if height[left] < height[right] {
+            if left_max <= height[left] {
+                left_max = height[left]
+            } else {
+                ans += left_max - height[left];
+            };
+            left += 1;
+        } else {
+            if right_max <= height[right] {
+                right_max = height[right];
+            } else {
+                ans += right_max - height[right];
+            }
+            right -= 1;
+        }
+    }
+    ans
+}
+
 
 #[cfg(test)]
 mod tests {
@@ -53,6 +93,14 @@ mod tests {
         assert_eq!(res, 2);
         let res = first_missing_positive(vec![7, 8, 9, 11, 12]);
         assert_eq!(res, 1);
+    }
+
+    #[test]
+    fn test_trap() {
+        let res = trap(vec![0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]);
+        assert_eq!(res, 6);
+        let res = trap(vec![]);
+        assert_eq!(res, 0);
     }
 }
 
