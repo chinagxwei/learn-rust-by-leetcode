56bfa5b40ec513a59d48fec717c043d75953d5c7
kumatata <chinagxwei@gmail.com>
2020-11-12 17:39:27 +0800

    change project

--- src/problems_1_to_10/problems_1_two_sum.rs
+++ src/problems_1_to_10/problems_1_two_sum.rs
@@ -23,14 +23,16 @@ pub struct Solution;
 impl Solution {
     pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
         let mut hm: HashMap<i32, i32> = HashMap::new();
-        for (i,v) in nums.into_iter().enumerate() {
-            if hm.contains_key(&(target - v)) {
-                return vec![*hm.get(&(target - v)).unwrap() as i32, i as i32]
-            }else {
-                hm.insert(v,i as i32);
+        let mut out: Vec<i32> = vec![];
+        for (index, value) in nums.into_iter().enumerate() {
+            if let Some(_x) = hm.get(&(target - value)) {
+                out.push(*hm.get(&(target - value)).unwrap());
+                out.push(index as i32);
+                break;
             }
+            hm.insert(value, index as i32);
         }
-        vec![]
+        out
     }
 }
 
